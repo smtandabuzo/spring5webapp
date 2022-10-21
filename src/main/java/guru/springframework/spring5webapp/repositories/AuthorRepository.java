9c815219541232edb6778e7621ffd6e9011ae186
sazi <sazi.mtandabuzo@ayo4u.com>
2022-10-21 16:15:13 +0200

    Entity changes

---
+++ src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,6 @@
+package guru.springframework.spring5webapp.repositories;
+
+import guru.springframework.spring5webapp.domain.Author;
+
+public interface AuthorRepository extends CrudRepository<Author, Long>{
+}
---
+++ src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
@@ -0,0 +1,6 @@
+package guru.springframework.spring5webapp.repositories;
+
+import guru.springframework.spring5webapp.domain.Book;
+
+public interface BookRepository extends CrudRepository<Book,Long>{
+}
