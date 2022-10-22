e21cc82142396a74918f2613ba56b6058495f850
sazi <sazi.mtandabuzo@ayo4u.com>
2022-10-22 11:51:58 +0200

    Author and Book changes

--- src/main/java/guru/springframework/spring5webapp/controllers/AuthorController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/AuthorController.java
@@ -1,2 +1,19 @@
-package guru.springframework.spring5webapp.controllers;public class AuthorController {
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+
+
+@Controller
+public class AuthorController {
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+        model.addAttribute("authors",authorRepository.findAll());
+        return "authors/list";
+    }
 }
--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -1,2 +1,18 @@
-package guru.springframework.spring5webapp.controllers;public class BookController {
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repositories.BookRepository;
+
+@Controller
+public class BookController {
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+        model.addAttribute("books",bookRepository.findAll());
+        return "books/list";
+    }
 }
